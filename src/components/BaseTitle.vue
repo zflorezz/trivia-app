efa4a485a2dbae163cb731c207e5f43b309a091c
zflorezz <zakinahf030904@gmail.com>
2024-02-23 13:02:53 -0600

    BaseTitle Working

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex items-center justify-center gap-4">
+        <h1 class="text-6xl font-thin text-slate-700 uppercase">
+            <slot/>
+        </h1>
+    </div>
+</template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hello There ✋')
+import BaseTitle from '@/components/BaseTitle.vue';
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <BaseTitle>TRIVIA APPLICATION</BaseTitle>
 </template>
