6fd187ba5291f7e84b82f4922d77b76221070781
zflorezz <zakinahf030904@gmail.com>
2024-02-23 15:51:19 -0600

    added style to categories

--- src/components/BaseTitle.vue
+++ src/components/BaseTitle.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex items-center justify-center gap-4">
-        <h1 class="text-6xl font-thin text-slate-700 uppercase">
+        <h1 class="text-6xl font-thin text-white uppercase">
             <slot/>
         </h1>
     </div>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,7 +14,15 @@ onMounted(async () => {
 
 <template>
   <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-  <div v-for="category in categories" :key="category.id">
-    {{ category.name }}  
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+    <RouterLink v-for="category in categories" 
+    :key="category.id"
+    :to="``"
+    class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-blue-300 py-4 font-bold uppercase hover:cursor-pointer hover:border-blue-400 hover:bg-blue-100 hover:text-white transition-colors duration-300"
+    >
+      {{ category.name }}  
+    </RouterLink>
   </div>
+
+ 
 </template>
