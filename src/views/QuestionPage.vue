49545715de3f5b21c01d6596cc71881c72513b40
zflorezz <zakinahf030904@gmail.com>
2024-03-04 20:48:11 -0600

    question and answers styling is done

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -47,13 +47,23 @@ const shuffle = (array) => {
 
 <template>
 
-<div v-if="question" class="">
-  <BaseTitle>{{ question.category }}</BaseTitle>
-  {{ question.question }}
+<div v-if="question" class="flex h-full w-full flex-col items-center gap-8 p-10">
+  <BaseTitle>{{  question.category }}</BaseTitle>
+  <!-- {{  question.question }} -->
 
-  <div v-for="answer in answers" v-html="answer.answer" :key="answer.id" class=""></div>
+  <div v-html="question.question" class="text-center text-2xl font-bold"></div>
+  <div class="grid w-full flex-grow grid-cols-2 gap-8">
+      <div v-for="answer in answers" 
+      v-html="answer.answer" 
+      :key="answer.id" 
+      class="bg-green-500 flex items-center justify-center text-4xl rounded-lg text-white py-10 px-2 ">
+
+      </div>
+
+  </div>
 </div>
 <div v-else class="">
   Loading...
 </div>
+
 </template>
