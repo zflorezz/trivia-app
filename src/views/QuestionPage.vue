9b9975e0889e3c424f60aa29e007058326dee6c5
zflorezz <zakinahf030904@gmail.com>
2024-03-04 19:49:54 -0600

    shuffle is working

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -25,7 +25,21 @@ onMounted(async () => {
 
   })
 
+  answers.value = shuffle(answers.value)
+  // console.log(question.value)
+
 })
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    //swap
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array
+}
+
 
 </script>
 
